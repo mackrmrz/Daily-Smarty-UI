81f7b887406a59a148eaa91987b8e000b71f76b0
Mack  Ramirez <mackramirez@Macks-MacBook-Pro.local>
2020-01-31 10:39:32 -0800

    Setup for Logo

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import Logo from "./logo";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Daily Smarty Grid Workout</h1>
-        <h2>React Redux</h2>
+        <Logo/>
       </div>
     );
   }
---
+++ src/components/logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Logo = () => {
+
+    return (
+        <div>
+            <img src="/assets/logo/dailysmarty.jpg"/>
+        </div>
+    )
+
+}
+
+export default Logo;
