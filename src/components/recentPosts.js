f30e776d32bd95a3ecf3e018bb2f24edc8dae476
Mack  Ramirez <mackramirez@Macks-MacBook-Pro.local>
2020-02-05 08:03:08 -0800

    fixed the lifecycle spelling

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { connect } from "react-redux";
 
 import * as actions from "../actions";
 
-const RecentPosts = () => {
-    return (
-        <div className="recent-posts">
-            <div className="recent-posts-wrapper">
-                <div className="recent-posts-heading"> Recent Posts </div>
-                <ul className="recent-posts-posts">
-                    <li>Post number 0</li>
-                    <li>Post number 1</li>
-                    <li>Post number 2</li>
-                    <li>Post number 3</li>
-                </ul>
+class RecentPosts extends Component {
+
+    componentDidMount() {
+        this.props.fetchRecentPosts();
+    }
+
+    render() {
+        return (
+            <div className="recent-posts">
+                <div className="recent-posts-wrapper">
+                    <div className="recent-posts-heading"> Recent Posts </div>
+                    <ul className="recent-posts-posts">
+                        <li>Post number 0</li>
+                        <li>Post number 1</li>
+                        <li>Post number 2</li>
+                        <li>Post number 3</li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    )
-    
+        )
+    }
 }
 
 export default connect(null, actions)(RecentPosts);
