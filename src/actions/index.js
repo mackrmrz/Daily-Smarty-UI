2f5c63aad451b75a4db5abc08c2d2e2db374ff5f
Mack  Ramirez <mackramirez@Macks-MacBook-Pro.local>
2020-02-05 11:49:15 -0800

    reduced with devtools

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,16 @@
+import { SET_RECENT_POSTS } from "./types";
+
+import axios from "axios";
+
 export function fetchRecentPosts() {
     return function(dispatch) {
-        // perform the request in here
-        console.log("Hi Mom");
+        axios.get('https://api.dailysmarty.com/posts')
+            .then(response => {
+                console.log("this is the data", response);
+                dispatch({ 
+                    type: SET_RECENT_POSTS,
+                    payload: response.data.posts
+                })
+            })
     }
 }
