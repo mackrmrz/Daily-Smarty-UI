702a6f4a1dfb5854225686a5b5dfc1a3d4ec70bf
Mack  Ramirez <mackramirez@Macks-MacBook-Pro.local>
2020-02-06 08:46:09 -0800

    Added Post component with map to associtated_topics

---
+++ src/components/post.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+
+class Post extends Component {
+
+    renderTopics() {
+        let topics = this.props.associated_topics.map((topic, index) => {
+            return <span className="post-topic" key={index}>{topic}</span>
+        })
+        return topics
+    }
+
+
+    render() {
+        return (
+        <li className="reccent-post"> 
+            <div className="recent-post-title">
+                {this.props.title} 
+            </div>
+            <div className="recent-post-topics">
+                {this.renderTopics()}
+            </div>
+            
+        </li>
+        )
+    }
+}
+
+export default Post;
